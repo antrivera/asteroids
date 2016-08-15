7ed12ac5074924416dd9adf8df79c1003a040d0c
App Academy Student <student@appacademy.io>
2016-08-15 15:42:49 -0700

    Asteroids successfully fly around

---
+++ asteroids.js
@@ -0,0 +1,19 @@
+const MovingObject = require('./lib/moving_object.js');
+const Util = require('./lib/util.js');
+const Asteroid = require('./lib/asteroid.js');
+const Game = require('./lib/game.js');
+const GameView = require('./lib/game_view.js');
+
+window.Util = Util;
+window.MovingObject = MovingObject;
+window.Asteroid = Asteroid;
+window.Game = Game;
+window.GameView = GameView;
+
+document.addEventListener("DOMContentLoaded", () => {
+  const canvas = document.getElementById('game-canvas');
+  let c = canvas.getContext("2d");
+  let game = new Game();
+  let gv = new GameView(game, c);
+  gv.start();
+});
---
+++ bundle.js
@@ -0,0 +1,218 @@
+/******/ (function(modules) { // webpackBootstrap
+/******/ 	// The module cache
+/******/ 	var installedModules = {};
+
+/******/ 	// The require function
+/******/ 	function __webpack_require__(moduleId) {
+
+/******/ 		// Check if module is in cache
+/******/ 		if(installedModules[moduleId])
+/******/ 			return installedModules[moduleId].exports;
+
+/******/ 		// Create a new module (and put it into the cache)
+/******/ 		var module = installedModules[moduleId] = {
+/******/ 			exports: {},
+/******/ 			id: moduleId,
+/******/ 			loaded: false
+/******/ 		};
+
+/******/ 		// Execute the module function
+/******/ 		modules[moduleId].call(module.exports, module, module.exports, __webpack_require__);
+
+/******/ 		// Flag the module as loaded
+/******/ 		module.loaded = true;
+
+/******/ 		// Return the exports of the module
+/******/ 		return module.exports;
+/******/ 	}
+
+
+/******/ 	// expose the modules object (__webpack_modules__)
+/******/ 	__webpack_require__.m = modules;
+
+/******/ 	// expose the module cache
+/******/ 	__webpack_require__.c = installedModules;
+
+/******/ 	// __webpack_public_path__
+/******/ 	__webpack_require__.p = "";
+
+/******/ 	// Load entry module and return exports
+/******/ 	return __webpack_require__(0);
+/******/ })
+/************************************************************************/
+/******/ ([
+/* 0 */
+/***/ function(module, exports, __webpack_require__) {
+
+	const MovingObject = __webpack_require__(1);
+	const Util = __webpack_require__(2);
+	const Asteroid = __webpack_require__(3);
+	const Game = __webpack_require__(4);
+	const GameView = __webpack_require__(5);
+
+	window.Util = Util;
+	window.MovingObject = MovingObject;
+	window.Asteroid = Asteroid;
+	window.Game = Game;
+	window.GameView = GameView;
+
+	document.addEventListener("DOMContentLoaded", () => {
+	  const canvas = document.getElementById('game-canvas');
+	  let c = canvas.getContext("2d");
+	  let game = new Game();
+	  let gv = new GameView(game, c);
+	  gv.start();
+	});
+
+
+/***/ },
+/* 1 */
+/***/ function(module, exports) {
+
+	function MovingObject(options) {
+	  this.pos = options.pos;
+	  this.vel = options.vel;
+	  this.radius = options.radius;
+	  this.color = options.color;
+	}
+
+	MovingObject.prototype.draw = function (ctx) {
+	  ctx.fillStyle = this.color;
+	  ctx.beginPath();
+
+	  ctx.arc(
+	    this.pos[0],
+	    this.pos[1],
+	    this.radius,
+	    0,
+	    2 * Math.PI,
+	    false
+	  );
+
+	  ctx.fill();
+	};
+
+	MovingObject.prototype.move = function () {
+	  this.pos[0] += this.vel[0];
+	  this.pos[1] += this.vel[1];
+	};
+
+	module.exports = MovingObject;
+
+
+/***/ },
+/* 2 */
+/***/ function(module, exports) {
+
+	const Util = {
+	  inherits(childClass, parentClass) {
+	    let Surrogate = function() {};
+	    Surrogate.prototype = parentClass.prototype;
+	    childClass.prototype = new Surrogate();
+	    childClass.prototype.constructor = childClass;
+	  },
+
+	  randomVec(length) {
+	    let x = Math.random(length);
+	    let y = Math.sqrt(Math.pow(length, 2) - Math.pow(x,2));
+	    return [x, y];
+	  }
+	};
+
+	module.exports = Util;
+
+
+/***/ },
+/* 3 */
+/***/ function(module, exports, __webpack_require__) {
+
+	const MovingObject = __webpack_require__(1);
+	const Util = __webpack_require__(2);
+
+	function Asteroid(options) {
+	  this.COLOR = 'red';
+	  this.RADIUS = 10;
+
+	  options.color = this.COLOR;
+	  options.radius = this.RADIUS;
+	  options.vel = Util.randomVec(Math.random(20));
+
+	  // super(options);
+	  MovingObject.call(this, options);
+	}
+
+	Util.inherits(Asteroid, MovingObject);
+
+	module.exports = Asteroid;
+
+
+/***/ },
+/* 4 */
+/***/ function(module, exports, __webpack_require__) {
+
+	const Asteroid = __webpack_require__(3);
+
+	function Game() {
+	  this.DIM_X = 900;
+	  this.DIM_Y = 900;
+	  this.NUM_ASTEROIDS = 10;
+	  this.asteroids = [];
+
+	  this.addAsteroids();
+	}
+
+	Game.prototype.randomPosition = function() {
+	  let x = Math.random() * this.DIM_X;
+	  let y = Math.random() * this.DIM_Y;
+	  console.log([x, y]);
+	  return [x, y];
+	};
+
+	Game.prototype.addAsteroids = function() {
+	  for (let i = 0; i < this.NUM_ASTEROIDS; i++) {
+	    let asteroid = new Asteroid({ pos: this.randomPosition() });
+	    this.asteroids.push(asteroid);
+	  }
+	};
+
+	Game.prototype.draw = function(ctx) {
+	  ctx.clearRect(0, 0, this.DIM_X, this.DIM_Y);
+
+	  this.asteroids.forEach(asteroid => {
+	    asteroid.draw(ctx);
+	  });
+	};
+
+	Game.prototype.moveObjects = function() {
+	  this.asteroids.forEach(asteroid => {
+	    asteroid.move();
+	  });
+	};
+
+	module.exports = Game;
+
+
+/***/ },
+/* 5 */
+/***/ function(module, exports, __webpack_require__) {
+
+	const Game = __webpack_require__(4);
+
+
+	function GameView(game, ctx) {
+	  this.game = game;
+	  this.ctx = ctx;
+	}
+
+	GameView.prototype.start = function() {
+	  setInterval(() => {
+	    this.game.moveObjects();
+	    this.game.draw(this.ctx);
+	  }, 20);
+	};
+
+	module.exports = GameView;
+
+
+/***/ }
+/******/ ]);
